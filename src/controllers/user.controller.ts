da301d9002f5daafb1cff29a551ceb300b5817f6
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-03-30 13:42:09 +0100

    users and functions ok

--- src/controllers/user.controller.ts
+++
@@ -1,7 +0,0 @@
-import {Request, Response} from "express";
-
-//Acciones y programacion
-
-export const getAllUsers = (req: Request, res: Response) => {
-    res.send("esto va fenómeno por mi mare")
-}
--- src/routes/user.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllUsers } from "../controllers/user.controller";
-
-const userRouter = Router();
-
-userRouter.get("/", getAllUsers);
-
-export default userRouter;
