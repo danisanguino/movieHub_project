8b45bc73ef029d4df737916fa6a4b7278b9f8f75
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-03-31 19:22:36 +0200

    puto node

--- src/controllers/genre.controller.ts
+++ src/controllers/genre.controller.ts
@@ -1,17 +1,27 @@
 import { Request, Response } from "express";
 import prisma from "../db/client";
 
-export const getAllGenre = (req: Request, res: Response) => {
+export const getAllGenre = async (req: Request, res: Response) => {
     try {
-        
+        const gettingGenres = await prisma.genre.findMany({
+            include: {genres: true}
+        });
+        res.status(201).send(gettingGenres);
     } catch (error) {
         res.status(404).send("Error to get genres")
     }
-    res.send("te traigo los generos bro")
 }
 
-export const createGenre = (req: Request, res: Response) => {
-    res.send("te he creado un género peliculero bro")
+export const createGenre = async(req: Request, res: Response) => {
+        const {name} = req.body
+        const { movieId} = req.params
+    try {
+        const newGenre = await prisma.genre.create({
+            data:{name, movies: { connect: {id:movieId}}}
+        });
+    } catch (error) {
+        
+    }
 }
 
 export const updateGenre = (req:Request, res: Response) => {
--- src/db/client.ts
+++ src/db/client.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 //No olvidar exportarlo
 export default prisma;
