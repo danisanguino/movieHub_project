d97afe176ab341d8ee91f2e9743df25ca9fdae73
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-04-02 10:17:02 +0200

    changes to switch branch

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,18 @@ import connect from "./db/db"
 const PORT = config.app.PORT
 
 
-expressApp.listen(PORT, async () =>{
-    try {
-        await prisma.$connect();
-        console.log(`Server is running on port ${PORT} and is connected to db MovieHub from Atlas`)
-    } catch (error) {
-        console.log("error to connect, try again")
-    }
-} )
+// expressApp.listen(PORT, async () =>{
+//     try {
+//         await prisma.$connect();
+//         console.log(`Server is running on port ${PORT} and is connected to db MovieHub from Atlas`)
+//     } catch (error) {
+//         console.log("error to connect, try again")
+//     }
+// } )
+
+connect().then(() => {
+    expressApp.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT} and is connected to Atlas data base`);
+    });
+});
 
